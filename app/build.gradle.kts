dcaebfa091d1423e5edf39f02338a73e3c228fc6
akexorcist <1949576+akexorcist@users.noreply.github.com>
2024-04-01 16:26:59 +0000

    bump: 1.0.2-dev01

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.akexorcist.ruammij"
         minSdk = 23
         targetSdk = 34
-        versionCode = 2
-        versionName = "1.0.1"
+        versionCode = 13
+        versionName = "1.0.2-dev01"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {

94939ec9af15411648e7b7c7a0822bad706a558c
Kittinun Vantasin <kittinun.f@gmail.com>
2024-04-06 18:05:09 +0900

    Add dark mode and adjust the API to use compose rule instead

--- app/src/test/java/com/akexorcist/ruammij/MainActivityTest.kt
+++ app/src/test/java/com/akexorcist/ruammij/MainActivityTest.kt
@@ -1,14 +1,17 @@
 package com.akexorcist.ruammij
 
-import android.R
-import androidx.test.core.app.ActivityScenario.launch
-import androidx.test.espresso.Espresso.onView
-import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onRoot
+import com.akexorcist.ruammij.ui.RuamMijApp
+import com.akexorcist.ruammij.ui.rememberAppState
+import com.akexorcist.ruammij.ui.theme.RuamMijTheme
 import com.akexorcist.ruammij.utils.SnapshotTests
 import com.github.takahirom.roborazzi.captureRoboImage
+import org.junit.Rule
 import org.junit.Test
 import org.junit.experimental.categories.Category
 import org.junit.runner.RunWith
+import org.koin.androidx.compose.KoinAndroidContext
 import org.koin.test.KoinTest
 import org.robolectric.RobolectricTestRunner
 import org.robolectric.annotation.Config
@@ -20,20 +23,69 @@ import org.robolectric.annotation.GraphicsMode
 @Category(SnapshotTests::class)
 class MainActivityTest : KoinTest {
 
+    @get:Rule
+    val composeTestRule = createComposeRule()
+
     @Test
     fun overview_en() {
-        val activityScenario = launch(MainActivity::class.java)
+        composeTestRule.setContent {
+            KoinAndroidContext {
+                RuamMijTheme {
+                    val appState = rememberAppState()
+                    RuamMijApp(appState = appState)
+                }
+            }
+        }
 
-        onView(withId(R.id.content))
+        composeTestRule.onRoot()
+            .captureRoboImage()
+    }
+
+
+    @Test
+    fun overview_dark_en() {
+        composeTestRule.setContent {
+            KoinAndroidContext {
+                RuamMijTheme(darkTheme = true) {
+                    val appState = rememberAppState()
+                    RuamMijApp(appState = appState)
+                }
+            }
+        }
+
+        composeTestRule.onRoot()
+            .captureRoboImage()
+    }
+
+    @Test
+    @Config(qualifiers = "+th")
+    fun overview_dark_th() {
+        composeTestRule.setContent {
+            KoinAndroidContext {
+                RuamMijTheme(darkTheme = true) {
+                    val appState = rememberAppState()
+                    RuamMijApp(appState = appState)
+                }
+            }
+        }
+
+        composeTestRule.onRoot()
             .captureRoboImage()
     }
 
     @Test
     @Config(qualifiers = "+th")
     fun overview_th() {
-        val activityScenario = launch(MainActivity::class.java)
+        composeTestRule.setContent {
+            KoinAndroidContext {
+                RuamMijTheme {
+                    val appState = rememberAppState()
+                    RuamMijApp(appState = appState)
+                }
+            }
+        }
 
-        onView(withId(R.id.content))
+        composeTestRule.onRoot()
             .captureRoboImage()
     }
 }

0ac6ba87aca520a3caa9e96dff978c51ff7a369a
Kittinun Vantasin <kittinun.f@gmail.com>
2024-04-06 14:30:10 +0900

    Don't commit if files isn't there

--- app/src/main/java/com/akexorcist/ruammij/ui/overview/OverviewScreen.kt
+++ app/src/main/java/com/akexorcist/ruammij/ui/overview/OverviewScreen.kt
@@ -37,7 +37,6 @@ import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.graphics.vector.rememberVectorPainter
 import androidx.compose.ui.platform.LocalContext

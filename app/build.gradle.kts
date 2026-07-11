9a3f031f202744118d25a00720111741fefcae79
akexorcist <1949576+akexorcist@users.noreply.github.com>
2026-07-11 20:42:34 +0000

    bump: 1.3.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,7 +23,7 @@ android {
         minSdk = libs.versions.minSdk.get().toInt()
         targetSdk = libs.versions.targetSdk.get().toInt()
         versionCode = 27
-        versionName = "1.2.1"
+        versionName = "1.3.0"
 
         testInstrumentationRunner = "androidx.test.runner.InstrumentationTestRunner"
         vectorDrawables {
